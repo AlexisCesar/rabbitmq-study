a398b72d8012b58716851ca16c0271b0eff79990
Alexis Almeida <alexis.cesar2017@outlook.com>
2023-03-28 20:34:01 -0300

    Add producer implementation

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("[START]");
+
+var factory = new ConnectionFactory {
+    HostName = "localhost"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "test-queue1",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Our message! :)";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+for(int i = 0; i < 100; i++)
+{
+    channel.BasicPublish(exchange: "test-exchange1",
+                    routingKey: String.Empty,
+                    basicProperties: null,
+                    body: body);
+    Console.WriteLine($"Sent message #{i + 1}: {message}");
+}
+
+Console.WriteLine("[END] Press any key to close...");
+Console.ReadKey();
