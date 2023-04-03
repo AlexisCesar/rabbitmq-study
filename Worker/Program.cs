7b41369f841e719cec3dd826fd3946e40e313e3f
Alexis Almeida <alexis.cesar2017@outlook.com>
2023-04-03 18:23:38 -0300

    Add distributed tasks through instances (load balancer)

---
+++ Worker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("[START]");
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "test-queue2",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+// Tells RabbitMQ to do not send another message until
+// this work has finished the previous one.
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Split( '.' ).Length - 1;
+    Thread.Sleep(dots * 1000);
+
+    Console.WriteLine("[x] Task done.");
+
+    // here channel could also be accessed as ((EventingBasicConsumer)sender).Model
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(queue: "test-queue2",
+                     autoAck: false,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
