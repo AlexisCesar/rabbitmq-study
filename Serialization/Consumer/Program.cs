2a8ba70830a7d94ffbe0af2e0f8137e232a8394f
Bruna Pisera Goncalves <pissara2@gmail.com>
2023-04-27 18:19:36 -0300

    Sending a serialized object

---
+++ Serialization/Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Text.Json;
+using Consumer;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("[START]");
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "serialization-queue",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+
+    var message = Encoding.UTF8.GetString(body);
+
+    var myObject = JsonSerializer.Deserialize<chuchu>(message);
+    Console.WriteLine($" [x] My object name {myObject.Name}");
+    Console.WriteLine($" [x] My object author {myObject.Author}");
+    Console.WriteLine($" [x] My object id {myObject.Id}");
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: "serialization-queue",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Serialization/Producer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Producer;
+using RabbitMQ.Client;
+
+Console.WriteLine("[START]");
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "serialization-queue",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+channel.ExchangeDeclare(exchange: "serialization-exchange",
+                        type: "fanout",
+                        durable: true,
+                        autoDelete: false);
+
+channel.QueueBind(queue: "serialization-queue",
+                  exchange: "serialization-exchange",
+                  routingKey: String.Empty,
+                  arguments: null);
+
+
+for (int i = 0; i < 100; i++)
+{
+    var banana = new banana()
+    {
+        Id = i,
+        Author2 = "test",
+        Description = "123",
+        Name = "456"
+    };
+
+    var message = JsonSerializer.Serialize(banana);
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "serialization-exchange",
+                    routingKey: String.Empty,
+                    basicProperties: null,
+                    body: body);
+    Console.WriteLine($"Sent message #{i + 1}: {message}");
+}
+
+Console.WriteLine("[END] Press any key to close...");
+Console.ReadKey();
